0368c33a11ad2e1db37984e1a452c2e62c76c7a6
Dmytro Petruk <bavaria95@gmail.com>
2018-03-26 23:03:52 +0200

    Class of Project to describe needed elements for the project

--- gcp.py
+++ gcp.py
@@ -1,5 +1,6 @@
 from point import Point
 
 class GCP(Point):
-    def __init__(self, x, y):
+    def __init__(self, x, y, label):
         super().__init__(x, y)
+        self.label = label
---
+++ project.py
@@ -0,0 +1,30 @@
+import xml.etree.ElementTree as ET
+from point import Point
+from gcp import GCP
+from processing_area import ProcessingArea
+
+class Project:
+    def __init__(self):
+        pass
+
+    def read_data_from_xml(self, filename):
+        self.filename = filename
+        self.tree = ET.parse(self.filename)
+        self.root = self.tree.getroot()
+
+        self._fill_data_from_xml()
+
+    def _fill_data_from_xml(self):
+        self.processing_area = self._read_processing_area()
+        self.gcps = self._read_gcps()
+
+    def _read_processing_area(self):
+        pa_el = self.root.find('./inputs/processingArea')
+        vertices = [Point(float(p.get('x')), float(p.get('y')))
+                    for p in pa_el.getchildren()]
+        return ProcessingArea(vertices)
+
+    def _read_gcps(self):
+        gcps_el = self.root.find('./inputs/gcps')
+        return [GCP(float(p.get('x')), float(p.get('y')), p.get('label'))
+                for p in gcps_el.getchildren()]
